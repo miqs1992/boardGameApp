177122ffe6c2526138ea603ddc2bb42e9f7ef01e
Wojciech Mikusek <miqs1992@gmail.com>
2019-02-24 18:09:50 +0100

    Add devise styles

--- app/javascript/packs/boardGameApp/components/landingPage.jsx
+++ app/javascript/packs/boardGameApp/components/landingPage.jsx
@@ -2,12 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class LandingPage extends React.Component {
+    logoutHandler = () => {
+        fetch("/logout", {
+            method: 'delete',
+            credentials: 'same-origin',
+            headers: {
+                "X-CSRF-Token": this.getMetaValue("csrf-token")
+            }
+        }).then(() => {
+            window.location.reload()
+        })
+    };
+
+    getMetaValue = (name) =>{
+        const element = document.head.querySelector(`meta[name="${name}"]`);
+        return element.getAttribute("content")
+    };
+
     render() {
         return(
             <div>
                 <h1>Hello World</h1>
-                <Link to="/test">test</Link><br/>
-                <a href="/login">go to login</a>
+                <button>
+                    <Link to="/test">test</Link>
+                </button><br/>
+                <button onClick={this.logoutHandler}>Log out</button>
             </div>
         )
     }
